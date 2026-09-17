6d79daae3393d8790129a4d348de4e6ddf52c1dc
DevGuru <guruseelan29aug2005@gmail.com>
2026-09-17 12:02:32 +0530

    Creating a Handler

---
+++ Backend/src/ResponseHandler.java
@@ -0,0 +1,24 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ResponseHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        // Response message
+        String response = "Welcome to the Wheater App!!";
+
+        // Send response headers (200 OK) and length
+        exchange.sendResponseHeaders(200, response.length());
+
+        //send Response body
+
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
